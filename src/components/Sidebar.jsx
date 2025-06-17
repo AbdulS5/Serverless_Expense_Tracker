31d1398e10477f18dd573df998ab180f2555efc4
Abdul Samad <abdulsamad11112222@gmail.com>
2025-06-16 20:59:43 -0500

    Added style to sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,16 +1,21 @@
-import { Link } from 'react-router-dom';
+import React from 'react';
+import {Link} from 'react-router-dom'
+import './Sidebar.css';
 
 function Sidebar() {
   return (
-    <div className="sidebar">
-      <h2>Your Name</h2>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/expenses">Expense List</Link></li>
-          <li><Link to="/chart">Expense Chart</Link></li>
+    <div className = "sidebar">
+      <div className="profile">
+        <img src="https://i.pravatar.cc/100" alt="User" className='profile-pic' />
+        <h3 className='username'>Abdul Samd</h3>
+      </div>
+      <div className="links">
+        <ul> 
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/expenses">Expense List</Link></li>
+            <li><Link to="/chart">Chart</Link></li>
         </ul>
-      </nav>
+      </div>
     </div>
   );
 }
